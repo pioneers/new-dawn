d29dba5c5f6d6f60c1266491938e68c320ad7c3a
Eddy Meals <edimea211@gmail.com>
2024-10-18 00:23:55 -0700

    Change window icon and title
    Change window icon from boilerplate placeholder to PiE logo.
    Change window title to `Dawn ${versionString}`.

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -312,6 +312,7 @@ export default function App() {
           setFieldIPAddress(data.fieldIPAddress);
           setFieldStationNum(data.fieldStationNumber);
           setShowDirtyUploadWarning(data.showDirtyUploadWarning);
+          document.getElementsByTagName('title')[0].innerText = `Dawn ${data.dawnVersion}`;
         }),
         window.electron.ipcRenderer.on(
           'renderer-battery-update',
