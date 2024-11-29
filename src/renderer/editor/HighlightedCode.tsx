7841683d51d1fae3f1646dfebefbd60d39f02d1b
Eddy Meals <edimea211@gmail.com>
2024-11-29 12:50:32 -0800

    Lint again

--- src/renderer/editor/HighlightedCode.tsx
+++ src/renderer/editor/HighlightedCode.tsx
@@ -2,6 +2,14 @@ import AceEditor from 'react-ace';
 import 'ace-builds/src-noconflict/mode-python';
 import './HighlightedCode.css';
 
+/**
+ * Uses a read-only AceEditor to display a code block with Python syntax highlighting. The only
+ * valid children of this component is text containing the code to be displayed. Common indentation
+ * is removed from each line of code before display. Only spaces are considered indentation; tabs,
+ * half-width spaces, and nonbreaking spaces are treated as content.
+ * @param props.indent - number of spaces of indentation to prepend to each line, after common
+ * indentation is removed.
+ */
 export default function HighlightedCode({
   children,
   indent = 0,
@@ -17,16 +25,19 @@ export default function HighlightedCode({
     lines.pop();
   }
   // Remove common indent
-  const minIndent = Math.min(...lines
-    .filter(line => line.trim().length)
-    .map(line => line.match(/^ */)[0].length)
+  const minIndent = Math.min(
+    ...lines
+      .filter((line) => line.trim().length)
+      .map((line) => line.match(/^ */)[0].length),
   );
-  const formatted = lines.map(line => ' '.repeat(indent) + line.slice(minIndent)).join('\n');
+  const formatted = lines
+    .map((line) => ' '.repeat(indent) + line.slice(minIndent))
+    .join('\n');
   return (
     <AceEditor
       value={formatted}
       className="HighlightedCode-editor"
-      readOnly={true}
+      readOnly
       showGutter={false}
       style={{ width: '100%' }}
       mode="python"
