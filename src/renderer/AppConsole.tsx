58ae439294f86e988ba68f6f45b8b4783cfa846e
zh3nl <zhenliu200677@gmail.com>
2025-03-10 16:13:50 -0700

    fixed failed tests for merging

--- src/renderer/AppConsole.tsx
+++ src/renderer/AppConsole.tsx
@@ -1,7 +1,6 @@
 import { useRef, useEffect, useState } from 'react';
 import AppConsoleMessage from '../common/AppConsoleMessage';
 import './AppConsole.css';
-import { contextIsolated } from 'process';
 
 /**
  * Component displaying output and error messages from student code ran on the robot.
