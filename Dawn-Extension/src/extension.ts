829f2d94422d070303588d5aa68f67757bae78d4
Samuel Agra Kim <samk31@berkeley.edu>
2026-08-01 20:59:04 -0700

    Add Main Features and New Configuration

--- Dawn-Extension/src/extension.ts
+++ Dawn-Extension/src/extension.ts
@@ -1,35 +1,116 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import CodeTransfer from './CodeTransfer';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// Codetransfer function to get info from settings
+function getCodeTransfer(): CodeTransfer | undefined {
+  const config = vscode.workspace.getConfiguration('dawn-VSCODE');
+
+  const port = config.get<number>("SSHPort")!;
+  const user = config.get<string>("SSHUser")!;
+  const pass = config.get<string>("SSHPassword")!;
+  let codepath = config.get<string>("CodePath")!;
+
+  if (!pass) {
+    vscode.window.showErrorMessage(
+      "Set dawn-VSCODE settings before uploading/downloading."
+    );
+    return undefined;
+  }
+  codepath = codepath.replace(/\$\{SSHUser\}/g, user);
+  return new CodeTransfer(codepath, port, user, pass);
+}
+  
+
+// take IP address given from setting otherwise default
+function getRobotIp(): string | undefined {
+  return vscode.workspace.getConfiguration('dawn-VSCODE').get<string>('robotIP');
+}
+
+
+// Extension Startup
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "dawn-VSCODE" is now active!');
+  context.subscriptions.push(
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const command = vscode.commands.registerCommand('dawn-VSCODE.UploadRobot', async () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		const outcome = await vscode.window.showInformationMessage(
-			'Test Information Outcome',
-			'Simulate Success',
-			'Simulate Failure'
-		);
+// Command for Upload
+    vscode.commands.registerCommand('dawn-VSCODE.UploadRobot', async () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showErrorMessage('No file open to upload.');
+        return;
+      }
+	  // save any unsave changes
+	  if (editor.document.isDirty) {
+		await editor.document.save();
+	  }
 
-		if (outcome === 'Simulate Success') {
-			vscode.window.showInformationMessage('Uploaded!');
-		} else if (outcome === "Simulate Failure") {
-			vscode.window.showInformationMessage('Fail to Upload!');
-		}
-	});
+	  const filePath = editor.document.uri.fsPath;
 
-	context.subscriptions.push(command);
+	// Error pop-up for when IP is not setup correctly
+	  const ip = getRobotIp();
+      if (!ip) {
+        vscode.window.showErrorMessage('Set dawn-VSCODE.robotIp in settings first.');
+        return;
+      }
+
+       const codeTransfer = getCodeTransfer();
+      if (!codeTransfer) {
+        return;
+      }
+
+	  try {
+        await codeTransfer.upload(filePath, ip);
+        vscode.window.showInformationMessage('Code uploaded successfully.');
+      } catch (e) {
+        vscode.window.showErrorMessage(`Failed to upload code: ${e}`);
+      }
+    }),
+
+// Command for Download
+	vscode.commands.registerCommand('dawn-VSCODE.DownloadRobot', async () => {
+
+      const ip = getRobotIp();
+      if (!ip) {
+        vscode.window.showErrorMessage('Set dawn-VSCODE.robotIp in settings first.');
+        return;
+      }
+
+        const codeTransfer = getCodeTransfer();
+      if (!codeTransfer) {
+        return;
+      }
+
+      try {
+        const content = await codeTransfer.download(ip);
+        const editor = vscode.window.activeTextEditor;
+
+        if (editor) {
+          const doc = editor.document;
+          const fullRange = new vscode.Range(
+            doc.positionAt(0),
+            doc.positionAt(doc.getText().length)
+          );
+
+          await editor.edit(editBuilder => {
+            editBuilder.replace(fullRange, content);
+          });
+
+          await doc.save();
+
+      } else {
+        const doc = await vscode.workspace.openTextDocument({
+          content,
+          language: 'python',
+        });
+        await vscode.window.showTextDocument(doc);
+      }
+      
+        vscode.window.showInformationMessage('Code downloaded successfully.');
+      } catch (e) {
+        vscode.window.showErrorMessage(`Failed to download code: ${e}`);
+      }
+    }),
+  );
 }
 
 // This method is called when your extension is deactivated
