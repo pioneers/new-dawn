f51730b677f4c05772b2a9c8640042212468dcfb
Samuel Agra Kim <samk31@berkeley.edu>
2026-08-11 01:01:35 -0700

    Add Main Features and New Configuration (VSCode Extension) (#83)
    * Add Main Features and New Configuration
    
    * Add Main Features and New Configuration
    
    * remove old plugin code
    
    * Changes made from suggestions such as implement new interface and remove redundant functions
    
    * Add try-catch error handling for download and upload

---
+++ Dawn-Extension/README.md
@@ -0,0 +1,19 @@
+# dawn-VSCODE README
+
+
+
+### 1.0.0
+
+// ## Features
+// Main features implemented
+// Ability to upload and download code from robot
+
+
+// ## Extension Settings
+
+## Newly Updated Configuration
+// - Settings Added
+// - Settings to change SSH features and IP address
+
+
+
---
+++ Dawn-Extension/src/extension.ts
@@ -0,0 +1,132 @@
+import * as vscode from 'vscode';
+import CodeTransfer from './CodeTransfer';
+
+// Interface to hold config settings
+interface ExtensionSettings {
+  port: number
+  user: string
+  pass: string
+  codepath: string
+  robotIp: string
+}
+
+
+function getExtensionSettings(): ExtensionSettings {
+  const config = vscode.workspace.getConfiguration('dawn-VSCODE');
+  const port = config.get<number>("SSHPort");
+  const user = config.get<string>("SSHUser")!;
+  const pass = config.get<string>("SSHPassword")!;
+  const codepath = config.get<string>("CodePath")!;
+  const robotIp = config.get<string>('robotIP')!;
+  if (!pass) {
+    throw new Error("SSH password must not be empty");
+  }
+  if (!robotIp) {
+    throw new Error("Robot IP must be set");
+  }
+  if (!port) {
+    throw new Error("SSH Port must be set");
+  }
+  if (!user) {
+    throw new Error("SSH Username must be set");
+  }
+  if (!codepath) {
+    throw new Error("File Code Path must be set");
+  }
+
+  return {
+    port: port,
+    user: user,
+    pass: pass,
+    robotIp: robotIp,
+    codepath: codepath,
+  };
+}
+
+
+// Extension Startup
+export function activate(context: vscode.ExtensionContext) {
+
+  context.subscriptions.push(
+
+    // Command for Upload
+    vscode.commands.registerCommand('dawn-VSCODE.UploadRobot', async () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showErrorMessage('No file open to upload.');
+        return;
+      }
+      // save any unsave changes
+      if (editor.document.isDirty) {
+        await editor.document.save();
+      }
+
+      const filePath = editor.document.uri.fsPath;
+
+      let settings: ExtensionSettings;
+      try {
+        settings = getExtensionSettings();
+      } catch (e) {
+        vscode.window.showErrorMessage(`${(e as Error).message}`);
+        return;
+      }
+
+      try {
+        const codeTransfer = new CodeTransfer(settings.codepath, settings.port, settings.user, settings.pass);
+        await codeTransfer.upload(filePath, settings.robotIp);
+        vscode.window.showInformationMessage('Code uploaded successfully.');
+      } catch (e) {
+        vscode.window.showErrorMessage(`Failed to upload code: ${e}`);
+      }
+    }),
+
+    // Command for Download
+    vscode.commands.registerCommand('dawn-VSCODE.DownloadRobot', async () => {
+
+      let settings: ExtensionSettings;
+      try {
+        settings = getExtensionSettings();
+      } catch (e) {
+        vscode.window.showErrorMessage(`${(e as Error).message}`);
+        return;
+      }
+
+
+      let content;
+      try {
+        const codeTransfer = new CodeTransfer(settings.codepath, settings.port, settings.user, settings.pass);
+        content = await codeTransfer.download(settings.robotIp);
+      } catch (e) {
+        vscode.window.showErrorMessage(`Failed to download code: ${e}`);
+        return;
+      }
+      const editor = vscode.window.activeTextEditor;
+
+      if (editor) {
+        const doc = editor.document;
+        const fullRange = new vscode.Range(
+          doc.positionAt(0),
+          doc.positionAt(doc.getText().length)
+        );
+
+        await editor.edit(editBuilder => {
+          editBuilder.replace(fullRange, content);
+        });
+
+        await doc.save();
+
+      } else {
+        const doc = await vscode.workspace.openTextDocument({
+          content,
+          language: 'python',
+        });
+        await vscode.window.showTextDocument(doc);
+      }
+
+      vscode.window.showInformationMessage('Code downloaded successfully.');
+    }),
+  );
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() { }
